15e53b9fbe173f52741182324958921aea0f6d13
luisjimenez24 <luis.jimenez4@alu.uclm.es>
2021-12-17 10:19:55 +0100

    Implementado método suma
    suma de 4 y 5

--- src/prueba_github/Principal.java
+++ src/prueba_github/Principal.java
@@ -3,5 +3,10 @@ package prueba_github;
 class Principal{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println(suma(4, 5));
+	}
+	
+	public static int suma (int a, int b) {
+		return a+b;
 	}
 }
