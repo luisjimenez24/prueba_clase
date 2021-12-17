07c2bb69876c9503662da4f7e1235d954def7aaf
luisjimenez24 <luis.jimenez4@alu.uclm.es>
2021-12-17 10:21:35 +0100

    Implementado método resta

--- src/prueba_github/Principal.java
+++ src/prueba_github/Principal.java
@@ -4,9 +4,13 @@ class Principal{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println(suma(4, 5));
+		System.out.println(resta(2, 1));
 	}
 	
 	public static int suma (int a, int b) {
 		return a+b;
+	}
+	public static int resta (int a, int b) {
+		return a-b;
 	}
 }
